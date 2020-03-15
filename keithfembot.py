3c23b9201ffa09bea3f7f2f67028077dd01f3678
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-03-16 00:12:26 +0100

    params to send

--- keithfembot.py
+++ keithfembot.py
@@ -13,13 +13,13 @@ gibberish_phrase = ["Good things come to those who wait.",
                     "A wise man once said, everything in its own time and place.",
                     "Fortune cookies rarely share fortunes."]
 
-def send(msg):
+def send(update, context, msg):
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def about(update, context):
     """ About """
-    send("Keith F'em, a community radio experiment, is presented by Keith in conjuction with SP2. `hello@keithfem.com`")
-    send("Author: `lmazzitelli@pm.me` - Coded in Barcelona during the COVID-19 outbreak.")
+    send(update, context, "Keith F'em, a community radio experiment, is presented by Keith in conjuction with SP2. `hello@keithfem.com`")
+    send(update, context, "Author: `lmazzitelli@pm.me` - Coded in Barcelona during the COVID-19 outbreak.")
 
 def echo(update, context):
     """ On noncommand i.e message - echo the message on Telegram"""
@@ -31,30 +31,30 @@ def error(update, context):
 
 def current(update, context):
     """ Displays the show that is on air at the moment. """
-    send("not done yet!")
+    send(update, context, "not done yet!")
 
 def now(update, context):
     """ Displays the show that is on air at the moment. """
-    send("not done yet!")
+    send(update, context, "not done yet!")
 
 def next(update, context):
     """ Displays the upcoming show. """
-    send("not done yet!")
+    send(update, context, "not done yet!")
 
 def today(update, context):
     """ Displays the schedule for today. """
-    send("not done yet!")
+    send(update, context, "not done yet!")
 
 def tomorrow(update, context):
     """ Displays the schedule for tomorrow. """
-    send("not done yet!")
+    send(update, context, "not done yet!")
 
 def week(update, context):
     """ Displays the schedule for the week. """
-    send("not done yet!")
+    send(update, context, "not done yet!")
 
 def gibberish(update, context):
-    send(random.choice(gibberish_phrase))
+    send(update, context, random.choice(gibberish_phrase))
 
 def help(update, context):
     """ Help usage. """
@@ -68,7 +68,7 @@ def help(update, context):
     `/gibberish`: some gibberish.
     `/help`: this help.
     """
-    send(help_text)
+    send(update, context, help_text)
 
 def main():
     """ Start the bot. """
