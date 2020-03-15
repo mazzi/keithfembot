cd31506f356be09af9d3012d62c5425adb73daf3
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-03-16 00:01:29 +0100

    encapsulation of send

--- keithfembot.py
+++ keithfembot.py
@@ -1,13 +1,25 @@
 from config import *
+import random
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+gibberish_phrase = ["Good things come to those who wait.",
+                    "Patience is a virtue.",
+                    "The early bird gets the worm.",
+                    "A wise man once said, everything in its own time and place.",
+                    "Fortune cookies rarely share fortunes."]
+
+def send(msg):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def about(update, context):
     """ About """
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    send("Keith F'em, a community radio experiment, is presented by Keith in conjuction with SP2. hello@keithfem.com")
+    send("Author: lmazzitelli@pm.me - Coded in Barcelona during the COVID-19 outbreak.")
 
 def echo(update, context):
     """ On noncommand i.e message - echo the message on Telegram"""
@@ -17,6 +29,33 @@ def error(update, context):
     """ Log Errors """
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+def current(update, context):
+    """ Displays the show that is on air at the moment. """
+    pass
+
+def now(update, context):
+    """ Displays the show that is on air at the moment. """
+    pass
+
+def next(update, context):
+    """ Displays the upcoming show. """
+    pass
+
+def today(update, context):
+    """ Displays the schedule for today. """
+    pass
+
+def tomorrow(update, context):
+    """ Displays the schedule for tomorrow. """
+    pass
+
+def week(update, context):
+    """ Displays the schedule for the week. """
+    pass
+
+def gibberish(update, context):
+    send(random.choice(gibberish_phrase))
+
 def main():
     """ Start the bot. """
     updater = Updater(token=HTTP_API_TOKEN, use_context=True)
@@ -24,17 +63,19 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-
     dp.add_handler(CommandHandler('about', about))
+    dp.add_handler(CommandHandler('current', current))
+    dp.add_handler(CommandHandler('now', now))
+    dp.add_handler(CommandHandler('next', next))
+    dp.add_handler(CommandHandler('today', today))
+    dp.add_handler(CommandHandler('tomorrow', tomorrow))
+    dp.add_handler(CommandHandler('week', week))
+    dp.add_handler(CommandHandler('gibberish', gibberish))
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 if __name__ == '__main__':
