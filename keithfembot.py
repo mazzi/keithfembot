0ef884617561c7236e0dbbb044e30ffd4694fa10
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-03-16 23:16:26 +0100

    a little asterisk

--- keithfembot.py
+++ keithfembot.py
@@ -99,7 +99,7 @@ def tomorrow(update, context):
             starts = show['starts'][-8:-3]
             ends = show['ends'][-8:-3]
             shows_msg += "(%s - %s) - *%s*\n" % (starts, ends, name,)
-        msg = "*Shows for %s _🇩🇪 time!_\n" % (day,)
+        msg = "Shows for %s _🇩🇪 time!_\n" % (day,)
         send(update, context, msg + shows_msg)
     else:
         send(update, context, "We cannot tell you at the moment.")
