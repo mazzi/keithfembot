77efe3f0a48f4d6f31ca8708b31fe351eed2b1f9
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-03-16 23:46:08 +0100

    config changed

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-BOT_NAME = os.environ.get("BOT_NAME", "")
 HTTP_API_TOKEN = os.environ.get("HTTP_API_TOKEN", "")
-KEITHFEM_BASE_URL = "https://keithfem2.airtime.pro/api/"
+KEITHFEM_BASE_URL = os.environ.get("KEITHFEM_BASE_URL", "")
