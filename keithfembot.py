7edd95bd973c2e3fb89ec5e9d5a7db47bca83ad2
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-03-23 21:08:49 +0100

    disabling echo for unknown commands

--- keithfembot.py
+++ keithfembot.py
@@ -140,7 +140,7 @@ def main():
     dp.add_handler(CommandHandler('week', week))
     dp.add_handler(CommandHandler('gibberish', gibberish))
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
 
     # Start the Bot
