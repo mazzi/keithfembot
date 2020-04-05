3f886fd4847a49c8c6f0e02637993fe98507fabd
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-04-05 20:21:34 +0200

    Formatting

--- keithfembot.py
+++ keithfembot.py
@@ -1,56 +1,77 @@
-from config import *
-import html
-import random
-import requests
-import datetime as dt
 import calendar
-from http import HTTPStatus
-from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime as dt
+import html
 import logging
+from http import HTTPStatus
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+import requests
+from config import FORTUNE_URL, HTTP_API_TOKEN, KEITHFEM_BASE_URL
+from telegram import ParseMode
+from telegram.ext import CommandHandler, Updater
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
+
 def send(update, context, msg):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=html.unescape(msg), parse_mode=ParseMode.MARKDOWN)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=html.unescape(msg),
+        parse_mode=ParseMode.MARKDOWN,
+    )
+
 
 def about(update, context):
     """ About """
-    msg = "Keith F'em, a community radio experiment, is presented by Keith in conjuction with SP2. `hello@keithfem.com`\n"
-    msg += "Bot created in Barcelona during the COVID-19 outbreak quarantine (March 2020) ✌"
+    msg = (
+        "Keith F'em, a community radio experiment, is presented by Keith "
+        "in conjuction with SP2. `hello@keithfem.com`\n"
+        "Bot created in Barcelona during the COVID-19 outbreak quarantine "
+        "(March 2020) ✌"
+    )
     send(update, context, msg)
+
 
 def error(update, context):
     """ Log Errors """
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
 def parse_show(show):
-    name = show['name']
-    starts = show['starts'][-8:-3]
-    ends = show['ends'][-8:-3]
-    return (starts, ends, name,)
+    name = show["name"]
+    starts = show["starts"][-8:-3]
+    ends = show["ends"][-8:-3]
+    return (
+        starts,
+        ends,
+        name,
+    )
+
 
 def show(update, context, when):
     """ Displays the show that is on air at the moment. """
     response = requests.get(KEITHFEM_BASE_URL + "live-info")
     if response.status_code == HTTPStatus.OK:
         response = response.json()
-        show = parse_show(response[when][0]) # just to shift the result
+        show = parse_show(response[when][0])  # just to shift the result
         send(update, context, "*%s* (%s - %s _🇩🇪 time!_)" % (show[2:] + show[:2]))
     else:
         send(update, context, "We cannot tell you at the moment.")
 
+
 def now(update, context):
     """ Displays the show that is in the air now. """
-    show(update,context, 'currentShow')
+    show(update, context, "currentShow")
+
 
 def next(update, context):
     """ Displays the upcoming show. """
-    show(update, context, 'nextShow')
+    show(update, context, "nextShow")
 
-def day(update,context, on_day):
+
+def day(update, context, on_day):
     """ Displays the schedule for the weekday on_day. """
     response = requests.get(KEITHFEM_BASE_URL + "week-info")
     if response.status_code == HTTPStatus.OK:
@@ -63,23 +84,28 @@ def day(update,context, on_day):
     else:
         send(update, context, "We cannot tell you at the moment.")
 
+
 def schedule_day(update, context, timedelta):
     my_date = dt.date.today() + dt.timedelta(days=timedelta)
     tomorrow = calendar.day_name[my_date.weekday()]
     day(update, context, tomorrow)
 
+
 def today(update, context):
     """ Displays the schedule for today. """
     schedule_day(update, context, timedelta=0)
+
 
 def tomorrow(update, context):
     """ Displays the schedule for tomorrow. """
     schedule_day(update, context, timedelta=1)
 
+
 def week(update, context):
     """ Displays the schedule for the week. """
     days_of_the_week = []
-    for day in range(0,7): days_of_the_week.append(calendar.day_name[day].lower())
+    for day in range(0, 7):
+        days_of_the_week.append(calendar.day_name[day].lower())
     response = requests.get(KEITHFEM_BASE_URL + "week-info")
     if response.status_code == HTTPStatus.OK:
         msg = ""
@@ -95,6 +121,7 @@ def week(update, context):
     else:
         send(update, context, "We cannot tell you at the moment.")
 
+
 def gibberish(update, context):
     """ Displays a fortune. """
     response = requests.get(FORTUNE_URL)
@@ -103,20 +130,24 @@ def gibberish(update, context):
     else:
         send(update, context, "Nothing to say about that.")
 
+
 def beer(update, context):
     """ Beer ad """
-    message = "I'm gonna plug this right here. " \
-        "Our good friends at Berliner Berg are delivering " \
-        "crate of beer to your door, AND all of the money " \
-        "goes to support Keith. It's really cool of them.\n" \
-        "[https://tinyurl.com/keithsolikiste]\n" \
-        "Anyway, if you wanna drink some pale ale, " \
+    message = (
+        "I'm gonna plug this right here. "
+        "Our good friends at Berliner Berg are delivering "
+        "crate of beer to your door, AND all of the money "
+        "goes to support Keith. It's really cool of them.\n"
+        "[https://tinyurl.com/keithsolikiste]\n"
+        "Anyway, if you wanna drink some pale ale, "
         "get it here and help us out. Love y'all!\n"
+    )
     send(update, context, message)
+
 
 def help(update, context):
     """ Help usage. """
-    help_text=(
+    help_text = (
         "`/about`: the old and boring about command.\n"
         "`/now`: show what is on the air at the moment.\n"
         "`/next`: displays the upcoming show.\n"
@@ -129,25 +160,27 @@ def help(update, context):
     )
     send(update, context, help_text)
 
+
 def main():
     """ Starts the bot. """
     updater = Updater(token=HTTP_API_TOKEN, use_context=True)
 
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler('about', about))
-    dp.add_handler(CommandHandler('now', now))
-    dp.add_handler(CommandHandler('next', next))
-    dp.add_handler(CommandHandler('today', today))
-    dp.add_handler(CommandHandler('tomorrow', tomorrow))
-    dp.add_handler(CommandHandler('week', week))
-    dp.add_handler(CommandHandler('gibberish', gibberish))
-    dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler('beer', beer))
+    dp.add_handler(CommandHandler("about", about))
+    dp.add_handler(CommandHandler("now", now))
+    dp.add_handler(CommandHandler("next", next))
+    dp.add_handler(CommandHandler("today", today))
+    dp.add_handler(CommandHandler("tomorrow", tomorrow))
+    dp.add_handler(CommandHandler("week", week))
+    dp.add_handler(CommandHandler("gibberish", gibberish))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("beer", beer))
     dp.add_error_handler(error)
 
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
