333b0c17d20d0807edd3579adde483ac2aa0db89
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2020-12-29 15:51:43 +0100

    Testing added

---
+++ src/keithfembot.py
@@ -0,0 +1,202 @@
+import calendar
+import datetime as dt
+import html
+import logging
+from http import HTTPStatus
+
+import requests
+from config import DADJOKE_URL, FORTUNE_URL, HTTP_API_TOKEN, KEITHFEM_BASE_URL
+from telegram import ParseMode
+from telegram.ext import CommandHandler, Updater
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+class KeithFemBotCommands:
+    def _send(self, update, context, msg):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=html.unescape(msg),
+            parse_mode=ParseMode.MARKDOWN,
+        )
+
+    def about(self, update, context):
+        """ About """
+        msg = (
+            "Keith F'em, a community radio experiment, is presented by Keith "
+            "in conjuction with SP2. `hello@keithfem.com`\n"
+            "Bot created in Barcelona during the COVID-19 outbreak quarantine "
+            "(March 2020) ✌"
+        )
+        self._send(update, context, msg)
+
+    def error(self, update, context):
+        """ Log Errors """
+        logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+    def _parse_show(self, show):
+        """Parses show parameters and scapes special characters. """
+        name = show["name"]
+        SPECIAL_MARKDONW_CHARS = "\\`*_{}[]()#+-.!"
+        for char in SPECIAL_MARKDONW_CHARS:
+            if char in name:
+                name = name.replace(char, "\\" + char)
+        starts = show["starts"][-8:-3]
+        ends = show["ends"][-8:-3]
+        return (
+            starts,
+            ends,
+            name,
+        )
+
+    def _show(self, update, context, when):
+        """ Displays the show that is on air at the moment. """
+        response = requests.get(KEITHFEM_BASE_URL + "live-info")
+        if response.status_code == HTTPStatus.OK:
+            response = response.json()
+            show = self._parse_show(response[when][0])  # just to shift the result
+            self._send(
+                update, context, "*%s* (%s - %s _🇩🇪 time!_)" % (show[2:] + show[:2])
+            )
+        else:
+            self._send(update, context, "We cannot tell you at the moment.")
+
+    def now(self, update, context):
+        """ Displays the show that is in the air now. """
+        self._show(update, context, "currentShow")
+
+    def next(self, update, context):
+        """ Displays the upcoming show. """
+        self._show(update, context, "nextShow")
+
+    def day(self, update, context, on_day):
+        """ Displays the schedule for the weekday on_day. """
+        response = requests.get(KEITHFEM_BASE_URL + "week-info")
+        if response.status_code == HTTPStatus.OK:
+            response = response.json()
+            shows_msg = ""
+            for show in response[on_day.lower()]:
+                shows_msg += "(%s - %s) - *%s*\n" % self._parse_show(show)
+            msg = "Shows for %s _🇩🇪 time!_\n" % (on_day.replace("next", ""),)
+            self._send(update, context, msg + shows_msg)
+        else:
+            self._send(update, context, "We cannot tell you at the moment.")
+
+    def _schedule_day(self, update, context, timedelta):
+        my_date = dt.date.today() + dt.timedelta(days=timedelta)
+        on_day = calendar.day_name[my_date.weekday()]
+        if timedelta and on_day == calendar.day_name[calendar.firstweekday()]:
+            on_day = "next" + on_day
+        self.day(update, context, on_day)
+
+    def today(self, update, context):
+        """ Displays the schedule for today. """
+        self._schedule_day(update, context, timedelta=0)
+
+    def tomorrow(self, update, context):
+        """ Displays the schedule for tomorrow. """
+        self._schedule_day(update, context, timedelta=1)
+
+    def week(self, update, context):
+        """ Displays the schedule for the week. """
+        days_of_the_week = []
+        for day in range(0, 7):
+            days_of_the_week.append(calendar.day_name[day].lower())
+        response = requests.get(KEITHFEM_BASE_URL + "week-info")
+        if response.status_code == HTTPStatus.OK:
+            msg = ""
+            response = response.json()
+            for day in response:
+                if day not in days_of_the_week:
+                    continue
+                msg += "*Shows for %s*\n" % (day.capitalize(),)
+                for show in response[day]:
+                    msg += "(%s - %s) - *%s*\n" % self._parse_show(show)
+            msg += "_ All shows are in 🇩🇪 time!_"
+            self._send(update, context, msg)
+        else:
+            self._send(update, context, "We cannot tell you at the moment.")
+
+    def gibberish(self, update, context):
+        """ Displays a fortune. """
+        response = requests.get(FORTUNE_URL)
+        if response.status_code == HTTPStatus.OK:
+            self._send(update, context, response.json())
+        else:
+            self._send(update, context, "Nothing to say about that.")
+
+    def joke(self, update, context):
+        """ Tells a dad joke. """
+        headers = {
+            "User-Agent": "Keith F'em Bot (https://github.com/mazzi/keithfembot)",
+            "Accept": "text/plain",
+        }
+        response = requests.get(DADJOKE_URL, headers=headers)
+        if response.status_code == HTTPStatus.OK:
+            self._send(update, context, response.text)
+        else:
+            self._send(update, context, "Nothing to say about that.")
+
+    def donate(self, update, context):
+        """" Displays donate link """
+        self._send(
+            update, context, "[https://www.paypal.me/keithfem]",
+        )
+
+    def togo(self, update, context):
+        """" Displays Keith togo link """
+        togo_text = (
+            "Keith Togo 🇹🇬\n"
+            "Thursday - Sunday 15-20h\n"
+            "Check https://t.me/keithtogo for more details\n"
+        )
+        self._send(update, context, togo_text)
+
+    def help(self, update, context):
+        """ Help usage. """
+        help_text = (
+            "`/about`: the old and boring about command.\n"
+            "`/now`: show what is on the air at the moment.\n"
+            "`/next`: displays the upcoming show.\n"
+            "`/today`: displays the schedule for today.\n"
+            "`/tomorrow`: displays the schedule for tomorrow.\n"
+            "`/week`: displays the shows for the week.\n"
+            "`/togo`: info to order drinks.\n"
+            "`/gibberish`: some gibberish.\n"
+            "`/joke`: KeithF'em BotMeister, tell me a joke.\n"
+            "`/donate`: donate to Keith F'em.\n"
+            "`/help`: this help.\n"
+        )
+        self._send(update, context, help_text)
+
+
+def main():
+    """ Starts the bot. """
+    updater = Updater(token=HTTP_API_TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    KFBC = KeithFemBotCommands()
+
+    dp.add_handler(CommandHandler("about", KFBC.about))
+    dp.add_handler(CommandHandler("now", KFBC.now))
+    dp.add_handler(CommandHandler("next", KFBC.next))
+    dp.add_handler(CommandHandler("today", KFBC.today))
+    dp.add_handler(CommandHandler("tomorrow", KFBC.tomorrow))
+    dp.add_handler(CommandHandler("week", KFBC.week))
+    dp.add_handler(CommandHandler("togo", KFBC.togo))
+    dp.add_handler(CommandHandler("gibberish", KFBC.gibberish))
+    dp.add_handler(CommandHandler("help", KFBC.help))
+    dp.add_handler(CommandHandler("joke", KFBC.joke))
+    dp.add_handler(CommandHandler("donate", KFBC.donate))
+    dp.add_error_handler(KFBC.error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_commands.py
@@ -0,0 +1,217 @@
+from unittest.mock import patch
+
+from freezegun import freeze_time
+from keithfembot import KeithFemBotCommands
+
+""" Methods without external dependencies """
+
+
+def test_about():
+
+    msg = (
+        "Keith F'em, a community radio experiment, is presented by Keith "
+        "in conjuction with SP2. `hello@keithfem.com`\n"
+        "Bot created in Barcelona during the COVID-19 outbreak quarantine "
+        "(March 2020) ✌"
+    )
+
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        KFBC = KeithFemBotCommands()
+        KFBC.about(update=None, context=None)
+        mock_send.assert_called_once_with(None, None, msg)
+
+
+def test_donate():
+    pass
+
+
+def test_togo():
+    pass
+
+
+def test_help():
+    pass
+
+
+""" Methods with external dependencies """
+
+
+def test_joke():
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            KFBC = KeithFemBotCommands()
+            KFBC.joke(update=None, context=None)
+            patched_get.assert_called_once()
+            mock_send.assert_called_once()
+
+
+def test_gibberish():
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            KFBC = KeithFemBotCommands()
+            KFBC.gibberish(update=None, context=None)
+            patched_get.assert_called_once()
+            mock_send.assert_called_once()
+
+
+@freeze_time("2020-12-29")
+def test_week(response_week_info):
+    msg = (
+        "*Shows for Monday*\n"
+        "(00:00 - 08:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(08:00 - 09:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(09:00 - 11:00) - *Caprisonne Revisited*\n"
+        "(11:00 - 13:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(13:00 - 14:00) - *The Keith Family ExperimentalHour*\n"
+        "(14:00 - 16:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(16:00 - 18:00) - *Liv and Ken play Heavy Rock for Moms*\n"
+        "(18:00 - 20:00) - *Still Hating*\n"
+        "(20:00 - 21:00) - *wood, metal, stoned*\n"
+        "(21:00 - 22:00) - *Ängstkiste*\n"
+        "(22:00 - 00:00) - *Novo line*\n"
+        "*Shows for Tuesday*\n"
+        "(00:00 - 01:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(01:00 - 02:00) - *The Matinee Radio Show Revisit*\n"
+        "(02:00 - 04:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(04:00 - 06:00) - *Window Magic&\\#039;s Transmission Service*\n"
+        "(06:00 - 08:00) - *Franktankerous&\\#039; Psychedelic Extravaganza Revisited*\n"
+        "(08:00 - 11:00) - *Last Week&\\#039;s Broth*\n"
+        "(11:00 - 13:00) - *Kraut Kontrol Revisited*\n"
+        "(13:00 - 14:00) - *The Keith Family Metal Hour*\n"
+        "(14:00 - 20:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(20:00 - 22:00) - *Hardcore Tuesdays*\n"
+        "*Shows for Wednesday*\n"
+        "(00:00 - 02:00) - *DJ MFK \\- Best of the 20Tens*\n"
+        "(02:00 - 04:00) - *El Melómano Alemán Revisit*\n"
+        "(04:00 - 06:00) - *Still Hating Revisited*\n"
+        "(11:00 - 13:00) - *Misunderestimated*\n"
+        "(13:00 - 15:00) - *Actual Figures*\n"
+        "(15:00 - 16:00) - *The Keith Family Punk Hour*\n"
+        "(16:00 - 18:00) - *Ambient Music Service for Sleepwaking*\n"
+        "(18:00 - 20:00) - *Kraut Kontrol*\n"
+        "(20:00 - 23:00) - *The Broth*\n"
+        "(23:00 - 01:00) - *The Boring Music Show*\n"
+        "*Shows for Thursday*\n"
+        "(01:00 - 02:00) - *The Matinee Radio Show Revisit*\n"
+        "(02:00 - 04:00) - *Franky Teardrop&\\#039;s Post\\-Victorian Classical Revisited*\n"
+        "(04:00 - 06:00) - *Caprisonne Revisited*\n"
+        "(08:00 - 10:00) - *Randy&\\#039;s Roadhouse Revisited*\n"
+        "(13:00 - 15:00) - *Snake Jazz*\n"
+        "(16:00 - 17:00) - *The Keith Family Indie Rock Hour*\n"
+        "(17:00 - 18:00) - *Talk To Me*\n"
+        "(20:00 - 22:00) - *The Pinnacle*\n"
+        "(22:00 - 02:00) - *Best of IntergalacticFM New Years Special 2021*\n"
+        "*Shows for Friday*\n"
+        "(02:00 - 03:00) - *Memory Foam with DJ Your Body*\n"
+        "(04:00 - 06:00) - *Still Hating Revisited*\n"
+        "(06:00 - 08:00) - *Faintfest Revisited*\n"
+        "(11:00 - 13:00) - *Misunderestimated*\n"
+        "(13:00 - 14:00) - *The Keith Family Blues Hour*\n"
+        "(14:00 - 16:00) - *Window Magic&\\#039;s Transmission Service*\n"
+        "(16:00 - 18:00) - *Slow Rise Radio*\n"
+        "(18:00 - 20:00) - *Sausage Man Radio*\n"
+        "(20:00 - 22:00) - *Randy&\\#039;s Roadhouse*\n"
+        "*Shows for Saturday*\n"
+        "(06:00 - 08:00) - *The Boring Music Show Revisit*\n"
+        "(08:00 - 10:00) - *Kraut Kontrol Revisited*\n"
+        "(10:00 - 12:00) - *The Cunning Ape*\n"
+        "(12:00 - 14:00) - *Misunderestimated*\n"
+        "(14:00 - 16:00) - *Snake Jazz Replay Saturday*\n"
+        "(20:00 - 22:00) - *Franktankerous&\\#039;s Psychedelic Extravaganza*\n"
+        "(22:00 - 00:00) - *Faintfest*\n"
+        "*Shows for Sunday*\n"
+        "(00:00 - 02:00) - *Hardcore Tuesday Revisits*\n"
+        "(02:00 - 04:00) - *Liv Me Alone Revisited*\n"
+        "(06:00 - 09:00) - *Ambiencia Maximatosis*\n"
+        "(14:00 - 16:00) - *El Melómano Alemán*\n"
+        "(16:00 - 18:00) - *Fuddle Duddle with DJ the Duncan*\n"
+        "(18:00 - 20:00) - *Ok, let&\\#039;s do it\\!*\n"
+        "(20:00 - 22:00) - *Keith Femme*\n"
+        "(22:00 - 00:00) - *Franky Teardrop&\\#039;s Post\\-Victorian Classical*\n"
+        "_ All shows are in 🇩🇪 time!_"
+    )
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            patched_get.return_value = response_week_info
+            KFBC = KeithFemBotCommands()
+            KFBC.week(update=None, context=None)
+
+            patched_get.assert_called_once()
+            mock_send.assert_called_once_with(None, None, msg)
+
+
+@freeze_time("2020-12-29")  # Wednesday
+def test_tomorrow(response_week_info):
+    msg = (
+        "Shows for Wednesday _🇩🇪 time!_\n"
+        "(00:00 - 02:00) - *DJ MFK \\- Best of the 20Tens*\n"
+        "(02:00 - 04:00) - *El Melómano Alemán Revisit*\n"
+        "(04:00 - 06:00) - *Still Hating Revisited*\n"
+        "(11:00 - 13:00) - *Misunderestimated*\n"
+        "(13:00 - 15:00) - *Actual Figures*\n"
+        "(15:00 - 16:00) - *The Keith Family Punk Hour*\n"
+        "(16:00 - 18:00) - *Ambient Music Service for Sleepwaking*\n"
+        "(18:00 - 20:00) - *Kraut Kontrol*\n"
+        "(20:00 - 23:00) - *The Broth*\n"
+        "(23:00 - 01:00) - *The Boring Music Show*\n"
+    )
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            patched_get.return_value = response_week_info
+            KFBC = KeithFemBotCommands()
+            KFBC.tomorrow(update=None, context=None)
+
+            patched_get.assert_called_once()
+            mock_send.assert_called_once_with(None, None, msg)
+
+
+@freeze_time("2020-12-29")  # Tuesday
+def test_today(response_week_info):
+    msg = (
+        "Shows for Tuesday _🇩🇪 time!_\n"
+        "(00:00 - 01:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(01:00 - 02:00) - *The Matinee Radio Show Revisit*\n"
+        "(02:00 - 04:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(04:00 - 06:00) - *Window Magic&\\#039;s Transmission Service*\n"
+        "(06:00 - 08:00) - *Franktankerous&\\#039; Psychedelic Extravaganza Revisited*\n"
+        "(08:00 - 11:00) - *Last Week&\\#039;s Broth*\n"
+        "(11:00 - 13:00) - *Kraut Kontrol Revisited*\n"
+        "(13:00 - 14:00) - *The Keith Family Metal Hour*\n"
+        "(14:00 - 20:00) - *Keith F&\\#039;em Bot DJ*\n"
+        "(20:00 - 22:00) - *Hardcore Tuesdays*\n"
+    )
+
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            patched_get.return_value = response_week_info
+            KFBC = KeithFemBotCommands()
+            KFBC.today(update=None, context=None)
+
+            patched_get.assert_called_once()
+            mock_send.assert_called_once_with(None, None, msg)
+
+
+def test_next(response_live_info):
+    msg = "*Franky Teardrop&\\#039;s Post\\-Victorian Classical* (22:00 - 00:00 _🇩🇪 time!_)"
+
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            patched_get.return_value = response_live_info
+            KFBC = KeithFemBotCommands()
+            KFBC.next(update=None, context=None)
+
+            patched_get.assert_called_once()
+            mock_send.assert_called_once_with(None, None, msg)
+
+
+def test_now(response_live_info):
+    msg = "*Keith F&\\#039;em Bot DJ* (20:00 - 22:00 _🇩🇪 time!_)"
+
+    with patch.object(KeithFemBotCommands, "_send", return_value=None) as mock_send:
+        with patch("requests.get") as patched_get:
+            patched_get.return_value = response_live_info
+            KFBC = KeithFemBotCommands()
+            KFBC.now(update=None, context=None)
+
+            patched_get.assert_called_once()
+            mock_send.assert_called_once_with(None, None, msg)
