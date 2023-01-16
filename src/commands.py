1ffae849fd9e0db4dd27d829fe802aa85b21de5e
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2023-01-16 16:38:05 -0300

    Adding parsing methods into classes (#227)
    * Removing helpers and tests. Adding parsing methods into classes

--- src/commands.py
+++ src/commands.py
@@ -1,10 +1,10 @@
 import calendar
 import datetime as dt
+import html
 
 from telegram import ParseMode
 
 from config import DADJOKE_URL, KEITHFEM_BASE_URL
-from helpers import ParseHelper
 
 
 class Command:
@@ -23,6 +23,37 @@ class Command:
             url=self.service_url,
             headers=self.headers,
         )
+
+    def _parse(self, show) -> str:
+        """Parses show name, start and end.
+
+        Args:
+            show (dict): A dictionary with a name and start and end of a show.
+
+        Returns:
+            tuple: A tuple with start time, end time and name of the show (un-scaped)
+        """
+
+        name = show["name"]
+        starts = show["starts"][-8:-3]
+        ends = show["ends"][-8:-3]
+        return (
+            starts,
+            ends,
+            html.unescape(name),
+        )
+
+    def _format(self, show) -> str:
+        """Formats a show info to be ready to be send
+
+        Args:
+            show (tuple): ("01:00", "04:00", "name")
+
+        Returns:
+            str: a show name in bold, and start and end time. Example:
+                Nocturnal Emissions (01:00 - 04:00 🇩🇪 time!)
+        """
+        return "*%s* (%s - %s _🇩🇪 time!_)" % (show[2:] + show[:2])
 
     def send(update, context, msg) -> None:
         """Send method from python-telegram-bot"""
@@ -103,73 +134,89 @@ class Joke(Command):
         return msg
 
 
-class Now(Command):
-    """Displays the show that is on air at the moment."""
+class ShowCommand(Command):
+    """Base class for single show display"""
 
-    def __init__(self, http_client, service_url=None, node=None):
+    def __init__(self, node, http_client, service_url=None):
         """Constructor
 
         Args:
+            node (str): A node in the response to be parsed.
             http_client (Any): mock compatible with requests
             service_url (str, optional): Url of the service to hit (airtime).
                 Defaults to KEITHFEM_BASE_URL + 'live-info'.
-            node (str, optional): A node in the response to be parsed.
-                Defaults to 'currentShow'.
         """
         super().__init__()
         self.http_client = http_client
         self.service_url = service_url or KEITHFEM_BASE_URL + "live-info"
-        self.node = node or "currentShow"
+        self.node = node
 
     def __call__(self, update, context) -> str:
-
         response = self._get().json()
-
-        helper = ParseHelper()
-        show = helper.parse_show(response[self.node][0])
-        msg = helper.format_show(show)
+        msg = self._format(self._parse(response[self.node][0]))
 
         self.send(update, context, msg)
         return msg
 
 
-class Next(Command):
+class Now(ShowCommand):
+    """Displays the show that is on air at the moment."""
+
+    def __init__(self, http_client, service_url=None, node="currentShow"):
+        super().__init__(
+            node=node,
+            http_client=http_client,
+            service_url=service_url,
+        )
+
+
+class Next(ShowCommand):
     """Displays the show that comes after the current one on air."""
 
-    def __init__(self, http_client, service_url=None, node=None):
-        super().__init__()
-        self.http_client = http_client
-        self.service_url = service_url or KEITHFEM_BASE_URL + "live-info"
-        self.node = node or "nextShow"
-
-    def __call__(self, update, context) -> str:
-
-        response = self._get().json()
-
-        helper = ParseHelper()
-        show = helper.parse_show(response[self.node][0])
-        msg = helper.format_show(show)
-
-        self.send(update, context, msg)
-        return msg
+    def __init__(self, http_client, service_url=None, node="nextShow"):
+        super().__init__(
+            node=node,
+            http_client=http_client,
+            service_url=service_url,
+        )
 
 
-class Today(Command):
-    """Displays the radio schedule for today"""
+class DayCommand(Command):
+    """Base class for daily show display"""
 
     def __init__(self, http_client, service_url=None):
         super().__init__()
         self.http_client = http_client
         self.service_url = service_url or KEITHFEM_BASE_URL + "week-info"
 
+    def _parse(self, response, day) -> str:
+        """From a response, it returns a string with the shows of the day
+
+        Args:
+            response (dict): A dict with the shows for the week.
+            day (str): day of the week to be parsed
+
+        Returns:
+            str: A string with all the shows for the day
+        """
+        shows_msg = ""
+        for show in response[day.lower()]:
+            shows_msg += "(%s - %s) - *%s*\n" % super()._parse(show)
+        return shows_msg
+
+
+class Today(DayCommand):
+    """Displays the radio schedule for today"""
+
+    def __init__(self, http_client, service_url=None):
+        super().__init__(http_client, service_url)
+
     def __call__(self, update, context) -> str:
         today = dt.date.today()
         on_day = calendar.day_name[today.weekday()].lower()
 
         response = self._get().json()
-
-        helper = ParseHelper()
-        shows = helper.parse_shows_for_day(response, on_day)
+        shows = self._parse(response, on_day)
 
         msg = "Shows for %s _🇩🇪 time!_\n" % (on_day.capitalize(),) + shows
 
@@ -177,17 +224,14 @@ class Today(Command):
         return msg
 
 
-class Tomorrow(Command):
+class Tomorrow(DayCommand):
     """Displays the radio schedule for tomorrow"""
 
     def __init__(self, http_client, service_url=None):
-        super().__init__()
-        self.http_client = http_client
-        self.service_url = service_url or KEITHFEM_BASE_URL + "week-info"
+        super().__init__(http_client, service_url)
 
     def __call__(self, update, context) -> str:
         tomorrow = dt.date.today() + dt.timedelta(days=1)
-
         on_day = calendar.day_name[tomorrow.weekday()].lower()
 
         # if tomorrow is monday, fetches 'nextmonday' on the array
@@ -195,9 +239,7 @@ class Tomorrow(Command):
             on_day = "next" + on_day
 
         response = self._get().json()
-
-        helper = ParseHelper()
-        shows = helper.parse_shows_for_day(response, on_day)
+        shows = self._parse(response, on_day)
 
         msg = (
             "Shows for %s _🇩🇪 time!_\n" % (on_day.replace("next", "").capitalize(),)
@@ -216,13 +258,34 @@ class Week(Command):
         self.http_client = http_client
         self.service_url = service_url or KEITHFEM_BASE_URL + "week-info"
 
+    def _parse(self, response) -> str:
+        """From a response, it returns the shows for the week
+
+        Args:
+            response (dict): A dict with the shows for the week.
+
+        Returns:
+            str: A string with all the shows for the week
+        """
+        msg = ""
+        days_of_the_week = []
+        for day in range(0, 7):
+            days_of_the_week.append(calendar.day_name[day].lower())
+
+        for day in response:
+            if day not in days_of_the_week:
+                continue
+            msg += "*Shows for %s*\n" % (day.capitalize(),)
+            for show in response[day]:
+                msg += "(%s - %s) - *%s*\n" % super()._parse(show)
+        msg += "_ All shows are in 🇩🇪 time!_"
+
+        return msg
+
     def __call__(self, update, context) -> str:
 
         response = self._get().json()
-
-        helper = ParseHelper()
-
-        msg = helper.parse_shows_for_week(response)
+        msg = self._parse(response)
 
         self.send(update, context, msg)
         return msg
